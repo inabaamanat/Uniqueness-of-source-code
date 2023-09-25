e3f9551b8771c7ea2db3c1c8a9bfbb74db896693
Quanggg03 <minhquang030120@gmail.com>
2023-09-25 00:59:52 -0500

    token_3_grams

--- main.py
+++ main.py
@@ -20,34 +20,32 @@ token_list = tokenize_to_set("second.py")
 def granularity_11(token_list):
     list_11granularity = []
 
-    for i in range(0, len(token_list), 10):
-        list_11granularity.append(token_list[i : i + 10])
+    for i in range(0, len(token_list), 11):
+        list_11granularity.append(token_list[i : i + 11])
 
     return list_11granularity
 
 
 granularity_11_list = granularity_11(token_list)
 # print(granularity_11_list)
-# print(granularity_11_list[1])
 
 
 def syntactic_redundancy_11(token_list):
-    result_list = []
+    tokens_3_grams = []
+    redundant = []
+
+    # Examine each list
     for lists in token_list:
-        for i in range(len(lists) - 1):
-            result = 0
-            for token1 in lists[i]:
-                for token2 in lists[i + 1]:
-                    if token1 == token2:
-                        result += 1
-                    else:
-                        result = result
-            syntactic_redundancy = result / len(lists[i])
-            result_list.append(syntactic_redundancy)
+        if len(lists) == 11:
+            for i in range(0, 9):
+                for j in range(i + 1, 10):
+                    for k in range(j + 1, 11):
+                        list_3grams = [lists[i], lists[j], lists[k]]
+                        tokens_3_grams.append(list_3grams)
 
-    return result_list
+    return tokens_3_grams
 
 
-list_syntactic_redundancy = syntactic_redundancy_11(token_list)
+list_syntactic_redundancy = syntactic_redundancy_11(granularity_11_list)
 
 print(list_syntactic_redundancy)
