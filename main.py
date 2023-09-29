278e3ff16f79a67485453498d2869db2a04dfb49
Minhh Quangg <quanggg@Minhhs-MBP.lan>
2023-09-28 22:07:37 -0500

    new feature 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tokenize
 
+#Tokenize the target project:
 
 def tokenize_to_set(filename):
     with tokenize.open(filename) as file:
@@ -16,36 +17,20 @@ def tokenize_to_set(filename):
 token_list = tokenize_to_set("second.py")
 # print(token_list)
 
+#ngrams the list of tokenize target project
+def tokenize_into_2_grams(token_list):
+    list_2_grams = []
 
-def granularity_11(token_list):
-    list_11granularity = []
+    for i in range(0, len(token_list), 2):
+        list_2_grams.append(token_list[i : i + 2])
 
-    for i in range(0, len(token_list), 11):
-        list_11granularity.append(token_list[i : i + 11])
+    return list_2_grams
 
-    return list_11granularity
-
-
-granularity_11_list = granularity_11(token_list)
-# print(granularity_11_list)
+print(tokenize_into_2_grams(token_list))
 
 
-def syntactic_redundancy_11(token_list):
-    tokens_3_grams = []
-    redundant = []
+#Tokenize the corpus
 
-    # Examine each list
-    for lists in token_list:
-        if len(lists) == 11:
-            for i in range(0, 9):
-                for j in range(i + 1, 10):
-                    for k in range(j + 1, 11):
-                        list_3grams = [lists[i], lists[j], lists[k]]
-                        tokens_3_grams.append(list_3grams)
+#ngrams the list of tokenize corpus
 
-    return tokens_3_grams
-
-
-list_syntactic_redundancy = syntactic_redundancy_11(granularity_11_list)
-
-print(list_syntactic_redundancy)
+#compare the to find the redundant
